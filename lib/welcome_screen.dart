9e14e74c2b2f26cebb1bdc2dd0bd708f0b5dc273
Steven Tran <steventran@Stevens-MacBook-Air.local>
2025-03-12 16:28:21 -0400

    Implement button to create cat profile.

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -8,6 +8,56 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _ageController = TextEditingController();
+  final TextEditingController _genderController = TextEditingController();
+  
+  void _catProfileDialog() {
+    _nameController.clear();
+    _ageController.clear();
+    _genderController.clear();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog (
+          title: Text('Enter Cat Info'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: 'Cat Name')
+              ),
+              TextField(
+                controller: _ageController,
+                decoration: InputDecoration(labelText: 'Cat Age')
+              ),
+              TextField(
+                controller: _genderController,
+                decoration: InputDecoration(labelText: 'Cat Gender')
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel')
+            ),
+            TextButton (
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Save')
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,10 +77,15 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               style: TextStyle(
                 fontSize: 30,
                 fontWeight: FontWeight.bold,
-              )
-              )
-          )
-        )
+              ),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => _catProfileDialog(),
+        icon: Icon(Icons.pets),
+        label: const Text("Create Cat Profile")
       )
     );
   }
