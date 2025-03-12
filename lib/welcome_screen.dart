bd2c5e33550418c1049ac52091b41f47e94d0c73
Steven Tran <steventran@Stevens-MacBook-Air.local>
2025-03-12 17:29:08 -0400

    Display cat profiles when created.

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+class CatProfile {
+  final String name;
+  final String age;
+  final String gender;
+
+  CatProfile({required this.name, required this.age, required this.gender});
+}
+
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
 
@@ -11,8 +19,9 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _ageController = TextEditingController();
   final TextEditingController _genderController = TextEditingController();
-  
-  void _catProfileDialog() {
+  final List<CatProfile> _catProfiles = [];
+
+  void _addProfile() {
     _nameController.clear();
     _ageController.clear();
     _genderController.clear();
@@ -20,22 +29,22 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return AlertDialog (
+        return AlertDialog(
           title: Text('Enter Cat Info'),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               TextField(
                 controller: _nameController,
-                decoration: InputDecoration(labelText: 'Cat Name')
+                decoration: InputDecoration(labelText: 'Cat Name'),
               ),
               TextField(
                 controller: _ageController,
-                decoration: InputDecoration(labelText: 'Cat Age')
+                decoration: InputDecoration(labelText: 'Cat Age'),
               ),
               TextField(
                 controller: _genderController,
-                decoration: InputDecoration(labelText: 'Cat Gender')
+                decoration: InputDecoration(labelText: 'Cat Gender'),
               ),
             ],
           ),
@@ -44,14 +53,21 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: Text('Cancel')
+              child: Text('Cancel'),
             ),
-            TextButton (
+            TextButton(
               onPressed: () {
+                setState(() {
+                  _catProfiles.add(CatProfile(
+                    name: _nameController.text,
+                    age: _ageController.text,
+                    gender: _genderController.text,
+                  ));
+                });
                 Navigator.of(context).pop();
               },
-              child: Text('Save')
-            )
+              child: Text('Save'),
+            ),
           ],
         );
       },
@@ -61,32 +77,68 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Align(
-        alignment: Alignment.topCenter,
-        child: Container(
-          margin: EdgeInsets.only(top: 100),
-          width: 350,
-          height: 130,
-          decoration: BoxDecoration(  
-            color: Colors.orangeAccent,
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Center(
-            child: Text(
-              'Welcome to CatCare!',
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: EdgeInsets.only(top: 100),
+              width: 350,
+              height: 130,
+              decoration: BoxDecoration(
+                color: Colors.orangeAccent,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Center(
+                child: Text(
+                  'Welcome to CatCare!',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ),
             ),
           ),
-        ),
+          Expanded(
+            child: GridView.builder(
+              padding: EdgeInsets.all(40),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemCount: _catProfiles.length,
+              itemBuilder: (context, index) {
+                final catProfileInfo = _catProfiles[index];
+                return Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 234, 176, 109),
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      height: 125,
+                      width: 125,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.pets, size: 50),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(catProfileInfo.name, style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => _catProfileDialog(),
+        onPressed: () => _addProfile(),
         icon: Icon(Icons.pets),
-        label: const Text("Create Cat Profile")
-      )
+        label: const Text("Create Cat Profile"),
+      ),
     );
   }
 }
