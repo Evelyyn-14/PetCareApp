037539782a72d49d476a634f9907cd67a9f9ab18
Steven Tran <steventran@Stevens-MacBook-Air.local>
2025-03-13 16:04:03 -0400

    Implement toggle light and dark mode.

--- lib/setting_screen.dart
+++ lib/setting_screen.dart
@@ -8,27 +8,61 @@ class SettingScreen extends StatefulWidget {
 }
 
 class _SettingScreenState extends State<SettingScreen> {
+  bool _nightMode = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              margin: EdgeInsets.only(top: 100),
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                color: Colors.orangeAccent,
-                borderRadius: BorderRadius.circular(100),
-              ),
-              child: Icon(Icons.pets, size: 100)
+    return MaterialApp(
+      theme: _nightMode ? ThemeData.dark() : ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => Null,
             ),
-          ),
-          SizedBox(height:20),
-          Text('Placeholder', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
-        ],
+        ),
+        body: Column(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                margin: EdgeInsets.only(top: 100),
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 244, 189, 118),
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: Icon(Icons.pets, size: 100),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text('Placeholder', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            Container(
+              margin: EdgeInsets.only(top: 50),
+              width: 300,
+              height: 400,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(228, 252, 239, 165),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 30),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        _nightMode = !_nightMode;
+                      });
+                    },
+                    icon: Icon(Icons.nightlight_round),
+                    label: Text('Toggle Light/Night Mode'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
