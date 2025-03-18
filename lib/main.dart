16b9af36526211dffa43818c18512737993d494d
Evelyynesco2014 <eescobedo1@student.gsu.edu>
2025-03-18 18:51:57 -0400

    Added Local Storage for Cat Profiles

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'setting_screen.dart';
+// import 'setting_screen.dart';
 import 'welcome_screen.dart';
 
 void main() {
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SettingScreen()
+      // home: SettingScreen(),
       home: WelcomeScreen(),
     );
   }
